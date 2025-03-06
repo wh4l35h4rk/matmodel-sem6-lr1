4756023ae47d44bd1e290e7f86a5afa77e72b23f
alice whaleshark <126924511+wh4l35h4rk@users.noreply.github.com>
2025-03-06 18:53:22 +1000

    add 2nd experiment

--- main.py
+++ main.py
@@ -25,8 +25,9 @@ def rungeKutta(x0, y0, h, param_list):
         x_list.append(x0)
         y_list.append(y)
 
-        if abs(y - y_prev) < 1e-5:
-            break
+        # if abs(y - y_prev) < 1e-5:
+        if abs(y - y_prev) < 1e-5 and x0 > 500:
+                break
     return x_list, y_list
 
 
@@ -68,6 +69,8 @@ def find_max(T0, P, S, k, epsilon, sigma):
 
 
 if __name__ == '__main__':
+    # ИССЛЕДОВАНИЕ 1
+
     T0 = 298 # 25 градусов цельсия
     P = 500 # 0.5 кВ
     S = 0.04
@@ -98,6 +101,67 @@ if __name__ == '__main__':
     plt.grid()
     plt.savefig('1.png')
     plt.show()
+    plt.clf()
 
+
+    # ИССЛЕДОВАНИЕ 2
+
+    T0 = 298 # 25 градусов цельсия
+    P = 500 # 0.5 кВ
+    S = 0.04
+    c = 904 # алюминий
+    m = 0.15
+    epsilon = 1
+    sigma = 5.67 * 10**(-8)
+    k = 6.706
+
+    y = T0
+
+    rk = rungeKutta(x0, y, h, [T0, P, S, c, m, k, epsilon, sigma])
+    x_list1 = rk[0][:(int)(500/h)]
+    y_list1 = rk[1][:(int)(500/h)]
+    T_max1 = y_list1[len(y_list1) - 1]
+
+    P = 1000
+
+    rk = rungeKutta(x0, y, h, [T0, P, S, c, m, k, epsilon, sigma])
+    x_list2 = rk[0]
+    y_list2 = rk[1]
+    T_max2 = y_list2[len(y_list2) - 1]
+
+    S = 0.1
+
+    rk = rungeKutta(x0, y, h, [T0, P, S, c, m, k, epsilon, sigma])
+    x_list3 = rk[0]
+    y_list3 = rk[1]
+    T_max3 = y_list3[len(y_list3) - 1]
+
+    epsilon = 0.5
+    P = 500
+    S = 0.04
+
+    rk = rungeKutta(x0, y, h, [T0, P, S, c, m, k, epsilon, sigma])
+    x_list4 = rk[0][:(int)(500/h)]
+    y_list4 = rk[1][:(int)(500/h)]
+    T_max4 = y_list4[len(y_list4) - 1]
+
+
+    plt.xlabel(r'$t$, с')
+    plt.ylabel(r'$T, \degree$K')
+
+    plt.plot(x_list1, y_list1, label=r'$P = 0.5 кВт, S = 0.04 м^2, \epsilon = 1$')
+    plt.plot(x_list2, y_list2, label=r'$P = 1 кВт, S = 0.04 м^2, \epsilon = 1$')
+    plt.plot(x_list3, y_list3, label=r'$P = 1 кВт, S = 0.1 м^2, \epsilon = 1$')
+    plt.plot(x_list4, y_list4, label=r'$P = 0.5 кВт, S = 0.04 м^2, \epsilon = 0.5$')
+
+    plt.plot(x_list1, [T_max1 for i in range(len(y_list1))], '--', linewidth=1)
+    plt.plot(x_list2, [T_max2 for i in range(len(y_list2))], '--', linewidth=1)
+    plt.plot(x_list3, [T_max3 for i in range(len(y_list3))], '--', linewidth=1)
+    plt.plot(x_list4, [T_max4 for i in range(len(y_list4))], '--', linewidth=1)
+
+    plt.legend(loc='lower right')
+    plt.grid()
+    plt.savefig('2.png')
+    plt.show()
 
 
